75ec2551503e984366d4c64c38f7b7b16ab6eb4d
tobia <tobiasad@gmail.com>
2026-07-29 11:59:28 +0200

    Stopped TAA progress, concidering different approaches

--- src/app/pathtracer/PathTracer.cpp
+++ src/app/pathtracer/PathTracer.cpp
@@ -362,7 +362,7 @@ void PathTracer::createPipelineInfo(){
 void PathTracer::loadMesh(const std::string& filename) {
 
     MeshLoader loader;
-    /*
+    
     loader.load("../assets/chair.obj", glm::vec3(2.0f, 0.f, 0.5f), .5f);
     loader.load("../assets/chair.obj", glm::vec3(4.0f, 0.f, 4.f), .5f);
     loader.load("../assets/chair.obj", glm::vec3(7.0f, 0.f, 1.f), .5f);
@@ -371,10 +371,8 @@ void PathTracer::loadMesh(const std::string& filename) {
 
     loader.load("../assets/smalltable.obj", glm::vec3(4.0f, 0.f, 3.f), .5f);
     loader.load("../assets/smalltable.obj", glm::vec3(7.0f, 0.f, 4.f), .5f);
-    */
-
-
-    loader.load("../assets/bunny.obj", glm::vec3(0.f, 0.3f, 0.5f), 5.0f);
+    
+    loader.load("../assets/bunny.obj", glm::vec3(4.f, 0.3f, 3.f), 5.0f);
 
     BVH bvh;
     bvh.build(loader.triangles);
